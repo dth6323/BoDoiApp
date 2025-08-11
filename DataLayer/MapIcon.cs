f32db76439abbcba04c403c49010814b5828a4dd
nguyen-bi-rain <vunguyen16052003@gmail.com>
2025-08-11 15:57:15 +0700

    load ảnh bản đồ

---
+++ DataLayer/MapIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+
+namespace BoDoiApp.DataLayer
+{
+    public class MapIcon
+    {
+        public PointF ImagePosition { get; set; }
+        public string Label { get; set; }
+        public Color IconColor { get; set; }
+        public int Size { get; set; }
+    }
+}
