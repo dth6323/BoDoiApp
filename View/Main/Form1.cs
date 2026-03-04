6d0f95f006c53e2c276ddb12f1716aaab7c94bac
hadz <dth6323@gmail.com>
2026-03-05 01:31:36 +0700

    test export

--- View/Main/Form1.cs
+++ View/Main/Form1.cs
@@ -1,15 +1,18 @@
 ﻿using BoDoiApp.View;
 using BoDoiApp.View.KhaiBaoDuLieuView;
 using BoDoiApp.View.Main;
+using DocumentFormat.OpenXml.Packaging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace BoDoiApp
 {
@@ -58,8 +61,47 @@ namespace BoDoiApp
 
         private void btn_kbdl_Click(object sender, EventArgs e)
         {
-            NavigationService.Navigate(new ThongTinTapBai());
+            //NavigationService.Navigate(new ThongTinTapBai());
+            string template = @"D:\code\BODOIAPP\Resources\word\word1.docx";
+            string output = @"D:\word2.docx";
 
+            ExportFromTemplate(
+                template,
+                output,
+                "noi dung 2",
+                "Nội dung thay cho $2"
+            );
+
+            MessageBox.Show("Xuất file thành công!");
+        }
+        public void ExportFromTemplate(
+    string templatePath,
+    string outputPath,
+    string danhGiaValue,
+    string value2)
+        {
+            // 1. Copy file gốc sang file mới
+            File.Copy(templatePath, outputPath, true);
+
+            // 2. Mở file mới để chỉnh sửa
+            using (WordprocessingDocument wordDoc =
+                WordprocessingDocument.Open(outputPath, true))
+            {
+                var texts = wordDoc.MainDocumentPart
+                                   .Document
+                                   .Descendants<Text>();
+
+                foreach (var text in texts)
+                {
+                    if (text.Text.Contains("{danhgia}"))
+                        text.Text = text.Text.Replace("{danhgia}", danhGiaValue);
+
+                    if (text.Text.Contains("$2"))
+                        text.Text = text.Text.Replace("$2", value2);
+                }
+
+                wordDoc.MainDocumentPart.Document.Save();
+            }
         }
     }
 }
